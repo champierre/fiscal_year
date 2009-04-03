3d38e330da8bc930dc5dfc986de4dabc46181f6f
Junya Ishihara <junya@champierre.com>
2009-04-03 14:31:08 +0900

    test code completed.

--- test/fiscal_year_test.rb
+++ test/fiscal_year_test.rb
@@ -1,11 +1,13 @@
-require 'test_helper'
+require 'test/unit'
+require 'test/test_helper'
+require File.dirname(__FILE__) + '/../lib/fiscal_year'
 
 class FiscalYearTest < ActiveSupport::TestCase
-  test "Should return previous year" do
-    assert_equal 2007, Date.new(2009, 3, 31).fiscal_year
+  def test_should_return_previous_year
+    assert_equal 2008, Date.new(2009, 3, 31).fiscal_year
   end
 
-  test "Should return current year" do
+  def test_should_return_current_year
     assert_equal 2009, Date.new(2009, 4, 1).fiscal_year
   end
 end
